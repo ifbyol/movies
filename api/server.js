296e2660874f51a14abd93effff5d1b888c1a69d
Nacho Fuertes <nacho@okteto.com>
2021-04-05 11:00:31 +0200

    Fix collection name for watching movies

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const app = express();
 const url = `mongodb://${process.env.MONGODB_USERNAME}:${encodeURIComponent(process.env.MONGODB_PASSWORD)}@${process.env.MONGODB_HOST}:27017/${process.env.MONGODB_DATABASE}`;
 
 function startWithRetry() {
-  mongo.connect(url, { 
+  mongo.connect(url, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     connectTimeoutMS: 1000,
@@ -40,7 +40,7 @@ function startWithRetry() {
 
       app.get("/api/watching", (req, res, next) => {
         console.log(`GET /api/watching`)
-        db.collection('movies').find().toArray( (err, results) =>{
+        db.collection('watching').find().toArray( (err, results) =>{
           if (err){
             console.log(`failed to query watching: ${err}`)
             res.json([]);
